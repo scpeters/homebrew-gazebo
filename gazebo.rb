7fc07743bf7a3750719c7d6f192f2d43d85e718c
Steven Peters <scpeters@osrfoundation.org>
2013-03-28 09:42:34 -0700

    gazebo.rb: initial commit
    Still trying to figure out all the dependencies, testing on
    clean virtual machine.

---
+++ gazebo.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Gazebo < Formula
+  homepage 'http://gazebosim.org'
+  url 'http://gazebosim.org/assets/distributions/gazebo-1.5.0.tar.bz2'
+  sha1 'f1b538e042a921cf58d06279b18a35d4e3ea8806'
+
+  depends_on 'boost'
+  depends_on 'cmake'  => :build
+  depends_on 'doxygen'
+  depends_on 'freeimage'
+  depends_on 'libtar'
+  depends_on 'ogre'
+  depends_on 'qt'
+  depends_on 'tbb'
+  depends_on 'tinyxml'
+
+  depends_on 'bullet' => :optional
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make install" # if this fails, try separate make/make install steps
+  end
+end
