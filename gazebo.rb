888b48608697b9a2f882f5ab07977e37b7fe434a
Steven Peters <scpeters@osrfoundation.org>
2013-04-04 09:49:58 -0700

    Update gazebo.rb
    Use newer commit on branch osx_hack, change options for bullet dependency.

--- gazebo.rb
+++ gazebo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gazebo < Formula
   homepage 'http://gazebosim.org'
-  url 'http://bitbucket.org/osrf/gazebo/get/62107b8d4aaa54c4929cd5296da665b25777da13.tar.bz2'
-  sha1 'fbd62c2535da115b98e6b4872465c23aa708edaa'
+  url 'http://bitbucket.org/osrf/gazebo/get/3a4e13ef0fa8f5fc0f1c480e013abf52c95f8faa.tar.bz2'
+  sha1 '70e4fd7176e123f4275e3a9c8ec3aae3ec6f6d40'
   version '1.6.0'
 
   depends_on 'boost'
@@ -19,7 +19,7 @@ class Gazebo < Formula
   depends_on 'tbb'
   depends_on 'tinyxml'
 
-  depends_on 'bullet' => :optional
+  depends_on 'bullet' => [:optional, 'shared', 'build-demo']
 
   def install
     ENV.m64
