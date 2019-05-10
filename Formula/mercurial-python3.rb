405a0b833b0e975635ef69ac1070ff3b033e6725
Steven Peters <scpeters@openrobotics.org>
2019-05-09 18:21:02 -0700

    mercurial-python3 5.0.0

---
+++ Formula/mercurial-python3.rb
@@ -0,0 +1,60 @@
+# No head build supported; if you need head builds of Mercurial, do so outside
+# of Homebrew.
+class MercurialPython3 < Formula
+  desc "Scalable distributed version control system"
+  homepage "https://mercurial-scm.org/"
+  url "https://www.mercurial-scm.org/release/mercurial-5.0.tar.gz"
+  sha256 "54ec76a0138d9b883873cfb2c651380659cd24aa99d92f8816982c6d0edd712a"
+
+  depends_on "python"
+
+  def install
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+    ENV["HGPYTHON3"] = "1"
+
+    system "make", "PREFIX=#{prefix}", "install-bin"
+
+    # Install chg (see https://www.mercurial-scm.org/wiki/CHg)
+    cd "contrib/chg" do
+      system "make", "PREFIX=#{prefix}", "HGPATH=#{bin}/hg", \
+             "HG=#{bin}/hg"
+      bin.install "chg"
+    end
+
+    # Configure a nicer default pager
+    (buildpath/"hgrc").write <<~EOS
+      [pager]
+      pager = less -FRX
+    EOS
+
+    (etc/"mercurial").install "hgrc"
+
+    # Install man pages, which come pre-built in source releases
+    man1.install "doc/hg.1"
+    man5.install "doc/hgignore.5", "doc/hgrc.5"
+
+    # install the completion scripts
+    bash_completion.install "contrib/bash_completion" => "hg-completion.bash"
+    zsh_completion.install "contrib/zsh_completion" => "_hg"
+  end
+
+  def caveats
+    return unless (opt_bin/"hg").exist?
+
+    cacerts_configured = `#{opt_bin}/hg config web.cacerts`.strip
+    return if cacerts_configured.empty?
+
+    <<~EOS
+      Homebrew has detected that Mercurial is configured to use a certificate
+      bundle file as its trust store for TLS connections instead of using the
+      default OpenSSL store. If you have trouble connecting to remote
+      repositories, consider unsetting the `web.cacerts` property. You can
+      determine where the property is being set by running:
+        hg config --debug web.cacerts
+    EOS
+  end
+
+  test do
+    system "#{bin}/hg", "init"
+  end
+end
